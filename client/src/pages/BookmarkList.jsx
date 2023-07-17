ee73d49cb261bb36dd14e1cf4fe10eeaef83cac2
strong-minsu <dlalstn5789@gmail.com>
2023-07-17 14:12:23 +0900

    Bookmark page - #8, #9

---
+++ client/src/pages/BookmarkList.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+
+import Header from "../components/Header";
+import Filter from "../components/Filter";
+import Card from "../components/Card";
+import Footer from "../components/Footer";
+function ProductsList(props) {
+  const state = useSelector((state) => state.products);
+  const [isMenuCilck, setIsMenuClick] = useState(false);
+  const [filterItem, setFilterItem] = useState("All");
+
+  function handleMenuClick() {
+    setIsMenuClick(!isMenuCilck);
+  }
+
+  function closeMenuClik() {
+    if (isMenuCilck) {
+      setIsMenuClick(false);
+    }
+  }
+  return (
+    <div onClick={closeMenuClik}>
+      <Header handleMenuClick={handleMenuClick} isMenuCilck={isMenuCilck} />
+      <Filter filterItem={filterItem} setFilterItem={setFilterItem} />
+      <div className="products-list-container">
+        <ul className="products-list">
+          {state.map((item) => {
+            if (props.bookmarkId.includes(item.id)) {
+              if (filterItem === "All" || item.type === filterItem) {
+                return (
+                  <Card
+                    key={item.id}
+                    item={item}
+                    bookmarkId={props.bookmarkId}
+                    setBookmarkId={props.setBookmarkId}
+                  />
+                );
+              }
+            }
+          })}
+        </ul>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+export default ProductsList;
